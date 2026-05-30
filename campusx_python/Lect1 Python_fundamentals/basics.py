5bc790c64c5e2f9024dbd18d61bbdefba412c79b
Nishant0225 <nishantkadu7020@gmail.com>
2026-05-31 02:12:17 +0530

    Add Python print() examples with sep and end parameters

--- campusx_python/Lect1 Python_fundamentals/basics.py
+++ campusx_python/Lect1 Python_fundamentals/basics.py
@@ -1,2 +1,9 @@
 # Hello World Program
 print("Hello World")
+
+# The sep='/' argument inserts a forward slash between each value.
+print('hello',101,'World',sep="/")
+
+# The end='-' argument replaces the default newline with a hyphen.
+print("hello",end='-')
+print("world")
