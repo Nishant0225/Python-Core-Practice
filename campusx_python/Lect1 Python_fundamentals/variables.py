d0617145c24f70521eeb1abab594b3ab0a49749b
Nishant0225 <nishantkadu7020@gmail.com>
2026-05-31 02:51:27 +0530

    feat: add Python variable concepts examples
    - Demonstrated basic variable declaration and usage
    - Performed arithmetic operations using variables
    - Showed dynamic binding by changing a variable's data type
    - Added notes on static binding in other languages
    - Demonstrated multiple variable assignment
    - Demonstrated assigning the same value to multiple variables

---
+++ campusx_python/Lect1 Python_fundamentals/variables.py
@@ -0,0 +1,26 @@
+#Basic Variable 
+name= 'Nishant'
+print(name)
+
+# Add the values of 'a' and 'b' and print the sum of them
+a=5
+b=6 
+print(a+b)
+
+#Dynamic Binding
+a=5
+print(a)
+a='Nishant'
+print(a)
+
+#Static Binding (used in other lang where the datatype cant be changed)
+# int a = 10
+# print(a)
+
+# Assign multiple values to multiple variables in a single statement
+a,b,c=1,2,3
+print(a,b,c)
+
+# Assign the same value to multiple variables
+a=b=c=5
+print(a,b,c)
