83e2ec217c5e1e78213c8508f2d45b86cd7280f4
Nishant0225 <nishantkadu7020@gmail.com>
2026-05-31 15:59:34 +0530

    feat: solve number swapping problem without special Python syntax
    - Accepted two numbers as input
    - Swapped values using a temporary variable
    - Displayed swapped values

---
+++ campusx_python/Lect1 Python_fundamentals/Task/Problem3.py
@@ -0,0 +1,10 @@
+# Q3: Take two numbers as input from the user and swap their values without using any special Python syntax.
+a=int(input('Enter first number'))
+b=int(input('Enter second number'))
+c=a
+c=a
+a=b
+b=c
+print("After swapping the numbers we get:")
+print("a =", a)
+print("b =", b)
