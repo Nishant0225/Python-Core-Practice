f97618fcc6d48f66c65ee6000ad57feca08d18f7
Nishant0225 <nishantkadu7020@gmail.com>
2026-06-01 01:23:27 +0530

    feat: print multiplication table using while loop

--- campusx_python/Lect2 Operators +if-Else+Loops/loops/while/tables.py
+++ campusx_python/Lect2 Operators +if-Else+Loops/loops/while/tables.py
@@ -3,3 +3,10 @@
 # ==========================
 
 # Q1: Write a program to print the multiplication table of a number entered by the user using a while loop.
+
+a=int(input("Enter a number"))
+b=1
+while b<11 :
+    print(a*b)
+    b=b+1
+        
