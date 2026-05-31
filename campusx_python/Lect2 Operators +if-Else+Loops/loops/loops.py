aab6e578081010d040ea295598162ff1e7a4f22f
Nishant0225 <nishantkadu7020@gmail.com>
2026-06-01 01:11:18 +0530

    task done

---
+++ campusx_python/Lect2 Operators +if-Else+Loops/loops/loops.py
@@ -0,0 +1,11 @@
+# In Python, there are two types of loops:
+# 1. While Loop
+# 2. For Loop
+
+# ==========================
+# While Loop Questions
+# ==========================
+
+# Q1: Write a program to print the multiplication table of a number entered by the user using a while loop.
+
+a=int(input("Enter a number"))
---
+++ campusx_python/Lect2 Operators +if-Else+Loops/loops/while/tables.py
@@ -0,0 +1,5 @@
+# ==========================
+# While Loop Questions
+# ==========================
+
+# Q1: Write a program to print the multiplication table of a number entered by the user using a while loop.
