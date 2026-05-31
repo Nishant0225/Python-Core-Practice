e412a4c70c8061486f967d932376d8c9fdfa3884
Nishant0225 <nishantkadu7020@gmail.com>
2026-06-01 03:39:15 +0530

    problem solved: print all pairs using nested loops

--- campusx_python/Lect2 Operators +if-Else+Loops/nestedloops/numberpattern.py
+++ campusx_python/Lect2 Operators +if-Else+Loops/nestedloops/numberpattern.py
@@ -1,3 +1,6 @@
+# Q5: Write a program using nested for loops to print all possible pairs of values (i, j),
+# where both i and j range from 1 to 4.
+
 for i in range(1,5):
     for j in range(1,5):
         print(i,j)
