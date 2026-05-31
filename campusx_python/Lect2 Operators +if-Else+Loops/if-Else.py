14d3a9db8c8d873f8f5d404a2de75374f3ea4ab8
Nishant0225 <nishantkadu7020@gmail.com>
2026-06-01 00:54:32 +0530

    topic done

--- campusx_python/Lect2 Operators +if-Else+Loops/if-Else.py
+++ campusx_python/Lect2 Operators +if-Else+Loops/if-Else.py
@@ -19,3 +19,17 @@ elif email== "tanvi@gmail.com" and password != "1234":
         print("Login failed")
 else:
     print("Login failed")
+    
+    
+#find min of 3 no
+aa=int(input("1st"))
+bb=int(input("2nd"))
+cc=int(input("3rd"))
+
+if aa>bb and aa>cc:
+    print('a is greatest')
+elif bb>cc:
+    print('b is greatest')
+else:
+    print('c is greatest')
+    
