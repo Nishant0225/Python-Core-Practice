110ac2a111a1949ddc5877234002cff959d7e26b
Nishant0225 <nishantkadu7020@gmail.com>
2026-05-31 15:52:19 +0530

    feat: solve Celsius to Fahrenheit conversion problem

---
+++ campusx_python/Lect1 Python_fundamentals/Task/Problem2.py
@@ -0,0 +1,6 @@
+# Q2: Write a program that converts a temperature from Celsius to Fahrenheit.
+# Formula: Fahrenheit =(Celsius × 9/5) + 32
+
+Celsius=float(input("Enter temperature in Celsius:"))
+Fahrenheit = (Celsius * 9/5) +32
+print("The Fahrenheit for the given Celsius is",Fahrenheit)
