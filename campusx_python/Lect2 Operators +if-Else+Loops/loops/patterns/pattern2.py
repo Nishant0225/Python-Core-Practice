690bd6687d2ea683ed5376954e57029b6d12fef3
Nishant0225 <nishantkadu7020@gmail.com>
2026-06-02 01:52:54 +0530

    task done

--- campusx_python/Lect2 Operators +if-Else+Loops/loops/patterns/pattern2.py
+++ campusx_python/Lect2 Operators +if-Else+Loops/loops/patterns/pattern2.py
@@ -6,5 +6,13 @@
 # 12321
 # 1234321
 
-
-for i in range(1,n+1)
+num=int(input('Enter Number of rows'))
+for i in range(1,num+1):
+    for j in range(1,i+1):
+        print(j,end="")
+    for k in range(i-1,0,-1):
+        print(k,end="")
+        
+    
+    print()
+    
---
+++ campusx_python/Lect3 Python Strings/pythonstrings.py
@@ -0,0 +1,12 @@
+# Strings in Python
+
+# Strings are a sequence of Unicode characters.
+
+# Topics Covered:
+# 1. Creating Strings
+# 2. Accessing Strings
+# 3. Adding Characters to Strings
+# 4. Editing Strings
+# 5. Deleting Strings
+# 6. Operations on Strings
+# 7. String Functions
