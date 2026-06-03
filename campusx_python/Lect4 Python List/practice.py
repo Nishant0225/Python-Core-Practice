5b7f605059b9fcf76b7e32cc626cb57d91b25dc5
Nishant0225 <nishantkadu7020@gmail.com>
2026-06-03 22:25:46 +0530

    task done

---
+++ campusx_python/Lect4 Python List/practice.py
@@ -0,0 +1,22 @@
+L = [[[1, 2], [3, 4]], [[5, 6], [7, 8]]]
+# to access 5
+print(L[1][0][0])
+# to access 2
+print(L[0][0][1])
+print(L[0][0][-1])
+
+# start to end
+S=[1,2,3,4,5,6,7]
+print(S[0:])
+
+# first 3
+print(S[0:3])
+
+# last 3
+print(S[-3:])
+
+# jump
+print(S[0::2])
+
+# reverse
+print(S[::-1])
