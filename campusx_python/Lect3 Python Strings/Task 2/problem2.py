dd85c2b0b9fdfc1ca81d6d79083f93a2ef1b3d07
Nishant0225 <nishantkadu7020@gmail.com>
2026-06-07 01:50:08 +0530

    task done

---
+++ campusx_python/Lect3 Python Strings/Task 2/problem2.py
@@ -0,0 +1,17 @@
+# Problem 12:
+# Take two strings as input from the user.
+# Insert the second string in the middle of the first string.
+# Print the resulting string.
+#
+# Example:
+# Input:
+# campusx
+# data
+#
+# Output:
+# camdatapusx
+
+a=input("Enter a string")
+b=input("Enter a string")
+p=a.index()/2
+print(p)
