1325531181a16745948405f9c381556f73e287dc
Nishant0225 <nishantkadu7020@gmail.com>
2026-06-08 01:47:33 +0530

    question done

--- campusx_python/Lect2 Operators +if-Else+Loops/loops/Task/problem9.py
+++ campusx_python/Lect2 Operators +if-Else+Loops/loops/Task/problem9.py
@@ -1,1 +1,10 @@
-# Problem 9: Write a program which will find all such numbers which are divisible by 7 but are not a multiple of 5, between 2000 and 3200 (both included). The numbers obtained should be printed in a comma-separated sequence on a single line.
+# Loop through all numbers from 2000 to 3200 (inclusive)
+for i in range(2000, 3201):
+
+    # Check if the number is divisible by 7
+    # and not divisible by 5
+    if i % 7 == 0 and i % 5 != 0:
+
+        # Print the number followed by a comma
+        # end="" prevents printing on a new line
+        print(i, ",", end="")
