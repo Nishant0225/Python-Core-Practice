18fa5a6adec368d12fc17027e0e0b75853dc45dd
Nishant0225 <nishantkadu7020@gmail.com>
2026-06-10 01:02:53 +0530

    problem solve sucessfully

---
+++ campusx_python/Lect2 Operators +if-Else+Loops/loops/Task/problem16.py
@@ -0,0 +1,14 @@
+#Print the following pattern. Write a program to use for loop to print the following reverse number pattern.
+
+# 5 4 3 2 1 
+# 4 3 2 1 
+# 3 2 1 
+# 2 1 
+# 1
+
+r=int(input("Enter Range"))
+for i in range(r,0,-1):
+    for j in range(i,0,-1):
+        print(j,end="")
+    print()
+    
