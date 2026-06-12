b7289ce5211fb272108437a2862cb111d1a73e86
Nishant0225 <nishantkadu7020@gmail.com>
2026-06-13 02:29:17 +0530

    problem pending

---
+++ campusx_python/Lect2 Operators +if-Else+Loops/loops/Task/problem22.py
@@ -0,0 +1,24 @@
+# Problem 7 - Find the sum of the series upto n terms.
+# Write a program to calculate the sum of series up to n term. For example, if n =5 the series will become 2 + 22 + 222 + 2222 + 22222 = 24690. Take the user input and then calculate. And the output style should match which is given in the example.
+
+# Example 1:
+
+# Input:`
+# 5
+
+# Output:
+
+# 2+22+222+2222+22222
+# Sum of above series is: 24690
+
+# n = int(input("Enter range"))
+# sum=2
+# a=1
+# for i in range(2,n+2,2):
+#     a=(i*10)
+#     sum=sum+a
+    
+#     print(a,":",sum)
+    
+    
+# # print("The sum of all terms will be :",sum)
---
+++ campusx_python/Lect2 Operators +if-Else+Loops/loops/Task/problem24.py
@@ -0,0 +1,18 @@
+# # Problem 9: Write a program that will take a decimal number as input and prints out the binary equivalent of the number
+# Number = 13
+# # 13 ÷ 2 → Quotient = 6, Remainder = 1
+# # 6 ÷ 2 → Quotient = 3, Remainder = 0
+# # 3 ÷ 2 → Quotient = 1, Remainder = 1
+# # 1 ÷ 2 → Quotient = 0, Remainder = 1
+
+# # Remainders collected: 1 0 1 1
+
+# # Read from bottom to top: 1101 (binary of 13).
+
+# n=int(input("Enter a number"))
+# c=0
+# for i in range()
+#     for i in range(1,n+1):
+#         if i%2==0:
+#             count+=1
+        
