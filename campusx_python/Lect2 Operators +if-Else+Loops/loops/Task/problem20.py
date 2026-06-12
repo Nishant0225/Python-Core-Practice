1e4d48fa91e41a4ff77904fecd6603d106296ab0
Nishant0225 <nishantkadu7020@gmail.com>
2026-06-13 01:40:42 +0530

    problems solved with proper comments

--- campusx_python/Lect2 Operators +if-Else+Loops/loops/Task/problem20.py
+++ campusx_python/Lect2 Operators +if-Else+Loops/loops/Task/problem20.py
@@ -1,11 +1,18 @@
-#  Write a Python Program to Find the Sum of the Series till the nth term:
-# 1 + x^2/2 + x^3/3 + … x^n/n
-# n will be provided by the user
-# x will be provided by the user
+# Program to find the sum of the series:
+# 1 + x^2/2 + x^3/3 + ... + x^n/n
 
-n=int(input("Enter value of a"))
-x=int(input("Enter value of x"))
-sum=1
-for i in range(2,n+1):
-    sum=sum+x**i/i
-print(sum)
+# Input the value of n (number of terms)
+n = int(input("Enter value of n: "))
+
+# Input the value of x
+x = int(input("Enter value of x: "))
+
+# Initialize the sum with the first term of the series
+sum = 1
+
+# Calculate and add the remaining terms of the series
+for i in range(2, n + 1):
+    sum = sum + x**i / i
+
+# Display the final sum
+print("Sum of the series =", sum)
