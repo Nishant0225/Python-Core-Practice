54d6ee4584ad5dd5248b50a94cb1c1921d0bdeb6
Nishant0225 <nishantkadu7020@gmail.com>
2026-06-13 01:19:11 +0530

    problem solved

--- campusx_python/Lect2 Operators +if-Else+Loops/loops/Task/problem19.py
+++ campusx_python/Lect2 Operators +if-Else+Loops/loops/Task/problem19.py
@@ -7,8 +7,8 @@
 
 a=int(input("Enter range"))
 for i in range(1,a+1):
-    for j in range(1,i+1):
-        for k in range(j,0,-1):
-            print(k,end="")
-        print()
+    for j in range(i,0,-1):
+        print(j,end="")
+    print()
+    
     
