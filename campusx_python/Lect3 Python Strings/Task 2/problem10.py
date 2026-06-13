836a46a9fe401b68b539eea867a923c96b89e96a
Nishant0225 <nishantkadu7020@gmail.com>
2026-06-14 04:10:02 +0530

    problem solved

--- campusx_python/Lect3 Python Strings/Task 2/problem10.py
+++ campusx_python/Lect3 Python Strings/Task 2/problem10.py
@@ -1,4 +1,21 @@
-# Problem 20: Write a program that can remove all the duplicate characters from a string. User will provide the input.
+# Problem 20:
+# Write a program to remove all duplicate characters from a string.
+
+# Take string input from the user
 str1 = input("Enter string: ")
-s=str1.split
-for i in range
+
+# Empty string to store characters without duplicates
+k = ""
+
+# Traverse each character of the input string
+for i in str1:
+
+    # Check if the character is not already present
+    # in the result string
+    if i not in k:
+
+        # Add the character to the result string
+        k = k + i
+
+# Print the string after removing duplicate characters
+print(k)
