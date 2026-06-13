bb8bcb988a7cc74ce326b43c1e57dddb9e5d2987
Nishant0225 <nishantkadu7020@gmail.com>
2026-06-14 01:06:12 +0530

    in process

---
+++ campusx_python/Lect2 Operators +if-Else+Loops/loops/Task/problem25.py
@@ -0,0 +1,14 @@
+# # Problem 10: Write a program that will take 2 numbers as input and prints the LCM and HCF of those 2 numbers
+
+# a=int(input("Enter Number"))
+# b=int(input("Enter Number"))
+# L=[]
+# M=[]
+# for i in range(1,a+1):
+#     if i%a==0:
+#         M.
+# for j in range(1,b+1):
+#     if j%b==0:
+#         M.append(j)
+# print(L)
+# print(M)
