2f7956eafd448d701ae82f836a6a1b39873e14a3
Nishant0225 <nishantkadu7020@gmail.com>
2026-06-20 04:28:53 +0530

    Create matrix using list comprehension

--- campusx_python/Lect4 Python List/Task/Problem13.py
+++ campusx_python/Lect4 Python List/Task/Problem13.py
@@ -1,5 +1,17 @@
 # #  Write a list comprehension to print the following matrix
 # # [[0, 1, 2], [3, 4, 5], [6, 7, 8]]
-# a=[[0, 1, 2], [3, 4, 5], [6, 7, 8]]
 
-# i for i in a
+# a = [[0, 1, 2],
+#      [3, 4, 5],
+#      [6, 7, 8]]
+
+# Given matrix
+a = [[0, 1, 2],
+     [3, 4, 5],
+     [6, 7, 8]]
+
+# Copy each row of matrix using list comprehension
+l = [i for i in a]
+
+# Print the matrix
+print(l)
---
+++ campusx_python/Lect4 Python List/Task/Problem14.py
@@ -0,0 +1,17 @@
+# Write a list comprehension that can transpose a given matrix
+# matrix = [
+# [1,2,3],
+# [4,5,6],
+# [7,8,9]
+# ]
+
+# [1, 4, 7]
+# [2, 5, 8]
+# [3, 6, 9]
+
+# matrix = [
+# [1,2,3],
+# [4,5,6],
+# [7,8,9]
+# ]
+# L=[j] for i in matrix for j in i
