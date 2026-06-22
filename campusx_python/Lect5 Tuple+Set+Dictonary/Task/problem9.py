7c604a6ae0cadda570bea8908a152d0de8694011
Nishant0225 <nishantkadu7020@gmail.com>
2026-06-23 03:40:49 +0530

    probllem solved

---
+++ campusx_python/Lect5 Tuple+Set+Dictonary/Task/problem9.py
@@ -0,0 +1,27 @@
+# # Q4: find union of n arrays.
+# # Example 1:
+
+# # Input:
+
+# # [[1, 2, 2, 4, 3, 6],
+# #  [5, 1, 3, 4],
+# #  [9, 5, 7, 1],
+# #  [2, 4, 1, 3]]
+# # Output:
+
+# # [1, 2, 3, 4, 5, 6, 7, 9]
+
+# n = int(input("Enter number of arrays: "))
+
+# arr = []
+
+# for i in range(n):
+#     a = list(map(int, input(f"Enter array {i+1}: ").split()))
+#     arr.append(a)
+    
+# s=set()
+# O=0
+#     for j in arr:
+#         s.add(j)
+#     O+=1
+# print(s)
