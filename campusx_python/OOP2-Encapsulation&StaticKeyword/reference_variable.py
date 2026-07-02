bbec22be3db96789ad4a6253bea999d5d8c646f7
Nishant0225 <nishantkadu7020@gmail.com>
2026-07-02 21:09:25 +0530

    topic done

--- campusx_python/OOP2-Encapsulation&StaticKeyword/reference_variable.py
+++ campusx_python/OOP2-Encapsulation&StaticKeyword/reference_variable.py
@@ -3,23 +3,36 @@
 # ==========================================
 
 # Definition:
-# A reference variable is a variable that stores the reference
-# (memory address) of an object.
-# It is used to access the object's data and methods.
+# A reference variable stores the reference (memory address)
+# of an object.
+# It is used to access the object's attributes and methods.
 
 # Syntax:
 # reference_variable = ClassName()
 
-# Example:
-
 class Student:
 
-    def __init__(self, name,gender):
-        self.name = name 
-        self.gender=gender
-# Outside the class -->Function 
-def greet(Student):
-    print('Hi my name is ',Student.name,'and I am a',Student.gender)
-    
-s=Student('Nishant','Male')
-p=greet(s)
+    # Constructor
+    def __init__(self, name, gender):
+        self.name = name
+        self.gender = gender
+
+
+# Outside the class --> Function
+def greet(student):      # student is a reference variable
+    print("Hi my name is", student.name, "and I am a", student.gender)
+
+    # Creating a new object
+    p1 = Student("Kumar", "Male")
+    return p1
+
+
+# Creating an object
+s = Student("Nishant", "Male")
+
+# Passing the reference variable to the function
+x = greet(s)
+
+# x stores the reference of the object returned by greet()
+print(x.name)
+print(x.gender)
