5d70b753364b03dd50c826e1cc06b710526649c4
Nishant0225 <nishantkadu7020@gmail.com>
2026-07-05 18:15:03 +0530

    feat: add Python program to perform addition of two numbers

---
+++ Python_DSA_Practice/Basics/add_two_num.py
@@ -0,0 +1,15 @@
+# ==========================================
+# Addition of Two Numbers
+# ==========================================
+
+# Assign value 10 to variable 'a'
+a = 10
+
+# Assign value 20 to variable 'b'
+b = 20
+
+# Add the values of 'a' and 'b' and display the result
+print("The sum of", a, "and", b, "is:", a + b)
+
+# Output:
+# The sum of 10 and 20 is: 30
